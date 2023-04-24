4ba65c12527da5bd7f80564468529fdbef8487cb
mateshwari <ganesh.medidi@think.design>
2023-04-25 02:05:45 +0530

    fixed build issue

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+// import { Provider } from 'react-redux';
+// import { store } from './store/store';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
+  // <Provider>
+ 
+  // </Provider>
 );
 
 
